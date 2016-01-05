76ecd9cff778e17a22554a2748b99b967754b069
iposton <ianposton@sbcglobal.net>
2016-01-05 12:08:54 -0800

    export middleware

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
 var express = require('express');
 var app = express();
 var port = 3000;
-
-
-var middleware = {
-	requireAuthentication: function (req, res, next) {
-		console.log('Private route hit!');
-		next();
-	},
-	logger: function (req, res, next) {
-		var date = new Date().toString();
-		console.log('Request: '+date+ ' ' +req.method+ ' ' + req.originalUrl);
-		next();
-	}
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 //app.use(middleware.requireAuthentication);
